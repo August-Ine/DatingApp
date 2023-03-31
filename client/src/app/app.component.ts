445b6c47ab163a3b89ac56b2359c085059241227
august269 <akirui95@gmail.com>
2023-03-31 09:29:14 +0300

    update to ng 15, client functionality:
    create home page, hook registration functionality to account service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +14,24 @@ export class AppComponent implements OnInit {
   users: any; //specify type any to hold list of users
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private accountServices: AccountService) {
 
   }
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+  getUsers() {
     this.http.get("https://localhost:7053/api/users/").subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log("Fetching users complete")
     });
-  }
-}
+  };
+  setCurrentUser() {
+    const userString: string | null = localStorage.getItem('user'); //read local storage for user
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountServices.setCurrentUser(user);
+  };
+};
