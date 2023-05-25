bed5f6eed25e59f1bc2aeae83ccf31c5ad7a7472
august269 <akirui95@gmail.com>
2023-05-25 16:18:58 +0300

    deployment ready

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -11,6 +11,7 @@ export class MemberMessagesComponent {
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
   messageContent = '';
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -19,8 +20,9 @@ export class MemberMessagesComponent {
 
   sendMessage() {
     if (!this.username) return;
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm?.reset();
-    })
+    }).finally(() => this.loading = false)
   }
 }
